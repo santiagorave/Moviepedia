e16af0f9675942a15fde9c4e0028ee5da91a74c9
Santiago Ramirez <santiagorave98@gmail.com>
2023-04-25 16:28:10 -0700

    Spinner config

---
+++ Moviepedia/src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+function LoadingSpinner(){
+    return (
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default LoadingSpinner
--- Moviepedia/src/components/getTop250.jsx
+++ Moviepedia/src/components/getTop250.jsx
@@ -1,31 +1,34 @@
 import { useState, useEffect } from 'react';
+import LoadingSpinner from './Spinner';
 function GetData() {
   const [movies, setMovies] = useState([]);
-  const [value, setValue] = useState(5);
+  const [isLoading, setLoading] = useState(true);
   // async function logJSONData() {
   //     let response = await fetch('/src/data/Top250.json');
   //        let data = await response.json();
   //        console.log(data.items)
   //   }
   //   logJSONData();
-  console.log(movies.slice(4, 9));
 
   const getElements = function () {
     fetch('/src/data/Top250.json').then((data) => {
       return data.json()
-    }).then(data => setMovies(data.items));
+    }).then(data => {setMovies(data.items); setLoading(false)});
   }
 
   useEffect(() => {
     getElements()
+
   }, [])
+  console.log(movies.slice(4, 9));
+
   return (
 
     <section className='movieContainer'>
       <h2>Top 250 Movies of All Time</h2>
       <article>
       <i className="fa-solid fa-chevron-left"></i>
-      {movies.length > 0 && (
+      {isLoading ? <LoadingSpinner/>: (
         <>
           {movies.slice(0, 6).map(movie => (
             <img className='poster' key={movie.id} src={movie.image} />
