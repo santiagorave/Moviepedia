8a515486dadc746b99e4c4f14d6664c2d77be652
Santiago Ramirez <santiagorave98@gmail.com>
2023-04-24 19:00:49 -0700

    Nav component that Leticia did in component way and get some json data

--- Moviepedia/src/App.jsx
+++ Moviepedia/src/App.jsx
@@ -1,26 +1,11 @@
 import { useState } from 'react'
+import Nav from './components/navComponent'
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-    
-    <nav>
-        <h4> Logo </h4>
-
-        <form action="#">
-            <section>
-                <input type="text" placeholder="" name="search"></input>
-                <button type="submit" id="searchBtn">
-                    <i className="fa-solid fa-magnifying-glass"></i>            
-                </button>
-            </section>
-        </form>
-        <aside>
-            <h4>Log in</h4>
-            <a href="">Profile</a> 
-        </aside>
-    </nav>
+    <Nav/>
     </>
   )
 }
---
+++ Moviepedia/src/components/navComponent.jsx
@@ -0,0 +1,21 @@
+function Nav(){
+    return(
+    <nav>
+        <h4> Logo </h4>
+
+        <form action="#">
+            <section>
+                <input type="text" placeholder="" name="search"></input>
+                <button type="submit" id="searchBtn">
+                    <i className="fa-solid fa-magnifying-glass"></i>            
+                </button>
+            </section>
+        </form>
+        <aside>
+            <h4>Log in</h4>
+            <a href="">Profile</a> 
+        </aside>
+    </nav>)
+}
+
+export default Nav;
