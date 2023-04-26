176240875ef8147d5421013f3794e2c858a01871
Santiago Ramirez <santiagorave98@gmail.com>
2023-04-26 14:32:34 -0700

    CSS testing and value test

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,12 +2,20 @@ function Modal(props) {
     const closeModal = function() {
         props.handler(false)
     }
-
+    // const getDetails = function () {
+    //     fetch('/src/data/Top250.json').then((data) => {
+    //       return data.json()
+    //     }).then(data => {setMovies(data.items); setLoading(false)});
+    //   }
     return (
          <div id="myModal" className="modal">
             <div className="modal-content">
                 <span onClick={closeModal} className="close">&times;</span>
-                <p>{props.movieData.title}</p>
+                <h3>{props.movieData.id}</h3>
+                <figure>
+                    <img className="posterDesc" src={props.movieData.image} alt={props.movieData.title} />
+                    <figcaption>{props.movieData.fullTitle}</figcaption>
+                </figure>
             </div>
         </div>
 
