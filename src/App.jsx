d74f10b175e1fb192d2b9de3e15a36756abb7823
Santiago Ramirez <santiagorave98@gmail.com>
2023-04-30 15:19:56 -0700

    Added final JSON to the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import Nav from './components/navComponent'
-import GetData from './components/getTop250'
+import GetData from './components/GetData'
 function App() {
 
   const GET_250_DATA="/src/data/Top250.json";
   const GET_POPULAR="/src/data/Popular.json";
+  const GET_TV="/src/data/Popular-TV.json";
+  const GET_SOON="/src/data/ComingSoon.json";
+
 
   return (
     <>
     <Nav/>
     <GetData title={"Most Popular"} data={GET_POPULAR}/>
+    <GetData title={"Popular TV Shows"} data={GET_TV}/>
     <GetData title={"Top 250 Movies of All Time"} data={GET_250_DATA}/>
+    <GetData title={"Coming Soon"} data={GET_SOON}/>
     </>
   )
 }
