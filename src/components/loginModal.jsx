0a88b7355bfab59735b5be2cd5ded63ee05bfd09
Letícia.guerra <84543651+LeticiaMatosGuerra@users.noreply.github.com>
2023-05-02 14:14:38 -0700

    loginModal & SignUpModal scss

--- src/components/loginModal.jsx
+++ src/components/loginModal.jsx
@@ -17,36 +17,44 @@ function LoginModal(props) {
         <>
             <section id="loginModal" className="loginModal">
 
-                {/* calls the function to close the modal */}
-                <span onClick={closeModal} className="close">&times;</span>
+                <article id='LoginContainer' className='LoginContainer'>
+                    {/* calls the function to close the modal */}
+                    <span onClick={closeModal} className="close">&times;</span>
 
-                <h4> Login </h4>
+                    <h4> Login </h4>
 
-                <form action="POST">
-                    <input type="email" name="email" id="email" />
-                    <input type="password" name="password" id="password" />
-                </form>
+                    <form action="POST">
+                        <input type="email" name="email" id="email" placeholder='your email' />
+                        <input type="password" name="password" id="password" placeholder='enter your password'/>
 
-                <article>
-                    <button type="submit">
-                        Login
-                    </button>
+                        <article className='loginBtn'>
+                            <button type="submit">
+                                Login
+                            </button>
+                        </article>
+
+                    </form>
+
+
+                    <aside>
+                        <section>
+                            <h5>
+                                Do not have an account? Create one
+                            </h5>
+                        </section>
+
+                        <section>
+                            <button onClick={() => {
+                                // when clicked: SignUp appers and Login hides
+                                props.modalSignUpState(true);
+                                props.modalLoginState(false);
+
+                            }}>
+                                Create User
+                            </button>
+                        </section>
+                    </aside>
                 </article>
-
-                <aside>
-                    <h6>
-                        Do not have an account? Create one
-                    </h6>
-
-                    <button onClick={() => {
-                        // when clicked: SignUp appers and Login hides
-                        props.modalSignUpState(true);
-                        props.modalLoginState(false);
-
-                    }}>
-                        Create User
-                    </button>
-                </aside>
 
             </section>
 
