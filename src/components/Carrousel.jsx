d2995934bc76fcb3260a523ea17d64e5b448022f
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-05-02 14:29:16 -0700

    centered slide pic and gave opacity to background

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -20,16 +20,19 @@ function CarrouselFig(props){
 function CarrouselSec(props){
     return(
         <section className={props.id}>
-            <h1>{props.title}</h1>
-            <div id="slides">
-                 <CarrouselFig id="slide1" src="https://lumiere-a.akamaihd.net/v1/images/p_disneymovies_avatarthewayofwater_1710_b7d39b03.jpeg"/>
-                 <CarrouselFig id="slide2" src="https://lumiere-a.akamaihd.net/v1/images/p_mulan_20529_83d3893a.jpeg?region=0%2C0%2C513%2C810"/>  
-                 <CarrouselFig id="slide3" src="https://lumiere-a.akamaihd.net/v1/images/p_cars_19643_4405006d.jpeg"/> 
-            </div>
-            <div id="navigation">
-                <i className="fa-solid fa-chevron-left" id={props.left}></i>
-                <i className="fa-solid fa-chevron-right" id={props.right}></i>
-            </div>
+            <article></article>
+            <section>
+                <h1>{props.title}</h1>
+                <div id="slides">
+                    <CarrouselFig id="slide1" src="https://lumiere-a.akamaihd.net/v1/images/p_disneymovies_avatarthewayofwater_1710_b7d39b03.jpeg"/>
+                    <CarrouselFig id="slide2" src="https://lumiere-a.akamaihd.net/v1/images/p_mulan_20529_83d3893a.jpeg?region=0%2C0%2C513%2C810"/>  
+                    <CarrouselFig id="slide3" src="https://lumiere-a.akamaihd.net/v1/images/p_cars_19643_4405006d.jpeg"/> 
+                </div>
+                <div id="navigation">
+                    <i className="fa-solid fa-chevron-left" id={props.left}></i>
+                    <i className="fa-solid fa-chevron-right" id={props.right}></i>
+                </div>
+            </section>
         </section>
     );
 }
