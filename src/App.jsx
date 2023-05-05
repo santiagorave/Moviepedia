14c2fd3e46fd821b2a8d6f5ee44f6aa390774341
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-05-04 19:41:48 -0700

    deleted Carrousel component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import Nav from './components/navComponent'
 import GetData from './components/getTop250'
 import MainCarrousel from './components/Carrouselv2'
-// import MainCarrousel from './components/Carrousel'
 
 function App() {
   // const [count, setCount] = useState(0)
