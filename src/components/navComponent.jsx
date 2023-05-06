65ab5ef71a0f491641d903154b5b1c66c69c3e9d
Santiago Ramirez <santiagorave98@gmail.com>
2023-05-05 17:46:50 -0700

    Search function almost done

--- src/components/navComponent.jsx
+++ src/components/navComponent.jsx
@@ -1,5 +1,8 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState,useEffect } from "react";
 function Nav(props){
+    const [allmovies, setAllMovies] = useState([]);
+    const [searchWord,setWord] = useState("");
 
     function LogInHandler(){
         if(props.userState.name){
@@ -8,17 +11,53 @@ function Nav(props){
             props.modalLoginState(true);
         }
     }
+    const getElements = function () {
+        fetch("src/data/Alldata.json").then(data => data.json()).then(movies => setAllMovies(movies));
+      }
+
+      
+  useEffect(() => {
+    getElements()
+
+  }, [])
+
+  const clickHandler = function(e) {
+    e.preventDefault();
+    console.log()
+  }
+  const inputHandler = function(e) {
+    setWord(e.target.value);
+
+  }
+
+//   console.log(allmovies.map(item => item.id))
+
+// console.log(allmovies.filter((movie) => movie.name?.toLowerCase().includes("incep")))
+    console.log(allmovies.filter(el=>el?.title.toLowerCase().includes("wick")).slice(0,3))
+    // console.log(allmovies.map(el=>el))
+
+
+
     return(
     <nav>
         <h1> Logo </h1>
 
         <form action="#">   
             <section>
-                <input type="text" placeholder="" name="search"></input>
-                <button type="submit" id="searchBtn">
+                <input onChange={inputHandler} autoComplete="off" type="text" placeholder="" name="search"></input>
+                <button type="click" id="searchBtn" onClick={clickHandler}>
                     <i className="fa-solid fa-magnifying-glass"></i>            
                 </button>
             </section>
+            <article className="searchBox">
+                {searchWord==""? null :
+                   allmovies.filter(el=>el?.title.toLowerCase().includes(searchWord.toLowerCase())).slice(0,3).map(el => {
+                    return (
+                        <p key={el.id} >{el.title}</p>
+                    )
+                   }) 
+                }
+            </article>
         </form>
         <aside >
             <Link hidden={!props.userState.name}  to="/profile">
